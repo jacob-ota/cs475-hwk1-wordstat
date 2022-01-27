0afb399e96c3a65e3ec2fc79dffb39a840750f5f
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-01-27 00:55:38 -0500

    Implemented user inputs and menu queries

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h> 
 #include "stats.h"
 #include "menu.h"
 
@@ -14,9 +15,43 @@
 int main(int argc, char **argv)
 {
 	char input[MAX_INPUT_LEN]; //holds user-input string
-
-	// TODO: repeatedly print menu options and prompt for an option
-	// TODO: display the proper output
+	char hashtag[MAX_INPUT_LEN] = "#";
+	bool notHashtag = true;
+	bool isFinished = false;
+	//get userinput until they enter a '#'
+	printf("Enter strings (Enter # to stop)\n");
+	while(notHashtag) {
+		scanf("%s", input);
+		if(strcmp(input, hashtag) == 0) {
+			printf("Move to menu \n");
+			notHashtag = false;
+		}
+	}
+	//start up the menu unless an exit is called
+	while(!isFinished) {
+		printf("\n");
+		// TODO: repeatedly print menu options and prompt for an option
+		int check = getMenuOption();
+		if(check == 1) {
+			printf("This is option 1!\n");
+		}
+		else if(check == 2) {
+			printf("This is option 2!\n");
+		}
+		else if(check == 3) {
+			printf("This is option 3!\n");
+		}
+		else if(check == 4) {
+			printf("This is option 4!\n");
+		}
+		else if(check == 5) {
+			isFinished = true;
+		}
+		else {
+			printf("Error: Unknown option %d. Try again!\n", check);
+		}
+		// TODO: display the proper output
+	}
 
 	printf("Exiting...\n");
 	return 0;
--- menu.c
+++ menu.c
@@ -7,4 +7,13 @@
 int getMenuOption()
 {
 	// TODO
+	int userOption;
+	printf("*** WORD STATS MENU ***\n");
+	printf("Enter 1 to print vowel and consonant frequency.\n");
+	printf("Enter 2 to print word count.\n");
+	printf("Enter 3 to print histogram.\n");
+	printf("Enter 4 to return to inputting more strings.\n");
+	printf("Enter 5 to quit.\n");
+	scanf("%d", &userOption);
+	return userOption;
 }
